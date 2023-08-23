24ff1c61567e804681fea37f72ce75781b6aabf9
SmartNaju <najrudeen458@gmail.com>
2023-08-23 23:43:30 +0530

    hello git

---
+++ src/main/java/org/testng/SampleTestNG/Hello.java
@@ -0,0 +1,15 @@
+package org.testng.SampleTestNG;
+
+import org.testng.annotations.Test;
+
+public class Hello {
+
+		
+	
+    @Test
+	private static void get() {
+		// TODO Auto-generated method stub
+		System.out.println("naju");
+	}
+
+}
---
+++ src/test/java/org/testNG/Naju.java
@@ -0,0 +1,12 @@
+package org.testNG;
+
+import org.testng.annotations.Test;
+
+public class Naju {
+	@Test
+	public void name() {
+		System.out.println("hello");
+		
+	}
+
+}
